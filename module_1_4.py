ca090c273510cb60e6a55ed85f125c5da33f6953
evgeniilisyanskii <evgeniilisyanskii@yandex.ru>
2024-08-18 00:13:14 +1000

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+my_string = input('Введите Ваше местр жительства: ' )
+print(my_string)
+print(len (my_string))
+name = 'Я привык бродить один'
+print(name)
+print(name.upper())
+print(name.lower())
+print(name.replace(' ',''))
+print(name.replace(' ','#'))
+print(name[0])
+print(name[-1])
